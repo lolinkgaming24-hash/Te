db1f85d6210438f7281bca8043e9e3f1a8eb7152
lolinkgaming24-hash <lolinkgaming24@gmail.com>
2026-04-25 08:01:02 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1030
+canvas.height = 580
 
 c.fillRect(0,0,canvas.width,canvas.height);
