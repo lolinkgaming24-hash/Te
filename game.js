0881807e051c7f02704eb01d0ded5ac24621c201
lolinkgaming24-hash <lolinkgaming24@gmail.com>
2026-04-25 08:08:02 +0800

    Refactor game.js by removing unused code
    Removed the rectangle drawing and modified the loop to log only the final value of i.

--- game.js
+++ game.js
@@ -4,9 +4,7 @@ const c = canvas.getContext("2d");
 canvas.width = 1030
 canvas.height = 582
 
-c.fillRect(0,0,canvas.width,canvas.height);
-
 for(let i = 0; i < 100; i++;){
-  console.log(i)
-  wait(1000)
-}
+  console.log(i);
+  wait(1000);
+};
